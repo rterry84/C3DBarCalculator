3c33d0f42c455156cb37c704a97ccdcb84a31aed
gherman825 <glen.herman@crimson3dx.com>
2013-10-11 09:12:25 -0500

    Restructured layout of controls using TableLayoutPanel

--- C3DBarCalculator/MainForm.Designer.cs
+++ C3DBarCalculator/MainForm.Designer.cs
@@ -31,7 +31,11 @@
             System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("Input Data", System.Windows.Forms.HorizontalAlignment.Left);
             System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("Bar Spacing", System.Windows.Forms.HorizontalAlignment.Left);
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tblLayMain = new System.Windows.Forms.TableLayoutPanel();
             this.grpInput = new System.Windows.Forms.GroupBox();
+            this.staEndSta = new C3DCommonCtrls.StationCtrl();
+            this.staBegSta = new C3DCommonCtrls.StationCtrl();
+            this.labBegSta = new System.Windows.Forms.Label();
             this.txtNumBars = new System.Windows.Forms.TextBox();
             this.labNumBars = new System.Windows.Forms.Label();
             this.txtBundleCnt = new System.Windows.Forms.TextBox();
@@ -46,18 +50,41 @@
             this.cmbSpaOptions = new System.Windows.Forms.ComboBox();
             this.barSzCtrl = new C3DCommonCtrls.BarSizeCtrl();
             this.labEndSta = new System.Windows.Forms.Label();
+            this.panCalcActions = new System.Windows.Forms.Panel();
+            this.btnSpacing = new System.Windows.Forms.Button();
             this.btnCalc = new System.Windows.Forms.Button();
             this.grpResults = new System.Windows.Forms.GroupBox();
             this.lstVwResults = new System.Windows.Forms.ListView();
             this.cHdrVariable = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.cHdrValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.btnSpacing = new System.Windows.Forms.Button();
-            this.labBegSta = new System.Windows.Forms.Label();
-            this.staBegSta = new C3DCommonCtrls.StationCtrl();
-            this.staEndSta = new C3DCommonCtrls.StationCtrl();
+            this.panActions = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.tblLayMain.SuspendLayout();
             this.grpInput.SuspendLayout();
+            this.panCalcActions.SuspendLayout();
             this.grpResults.SuspendLayout();
+            this.panActions.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tblLayMain
+            // 
+            this.tblLayMain.ColumnCount = 1;
+            this.tblLayMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblLayMain.Controls.Add(this.grpResults, 0, 2);
+            this.tblLayMain.Controls.Add(this.grpInput, 0, 0);
+            this.tblLayMain.Controls.Add(this.panCalcActions, 0, 1);
+            this.tblLayMain.Controls.Add(this.panActions, 0, 3);
+            this.tblLayMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblLayMain.Location = new System.Drawing.Point(0, 0);
+            this.tblLayMain.Name = "tblLayMain";
+            this.tblLayMain.RowCount = 4;
+            this.tblLayMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 271F));
+            this.tblLayMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tblLayMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblLayMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tblLayMain.Size = new System.Drawing.Size(522, 735);
+            this.tblLayMain.TabIndex = 4;
             // 
             // grpInput
             // 
@@ -78,12 +105,37 @@
             this.grpInput.Controls.Add(this.cmbSpaOptions);
             this.grpInput.Controls.Add(this.barSzCtrl);
             this.grpInput.Controls.Add(this.labEndSta);
-            this.grpInput.Location = new System.Drawing.Point(12, 12);
+            this.grpInput.Location = new System.Drawing.Point(3, 3);
             this.grpInput.Name = "grpInput";
-            this.grpInput.Size = new System.Drawing.Size(496, 267);
-            this.grpInput.TabIndex = 0;
+            this.grpInput.Size = new System.Drawing.Size(516, 264);
+            this.grpInput.TabIndex = 1;
             this.grpInput.TabStop = false;
             this.grpInput.Text = "Input Options";
+            // 
+            // staEndSta
+            // 
+            this.staEndSta.Location = new System.Drawing.Point(334, 20);
+            this.staEndSta.Name = "staEndSta";
+            this.staEndSta.Size = new System.Drawing.Size(111, 30);
+            this.staEndSta.Station = 0D;
+            this.staEndSta.TabIndex = 17;
+            // 
+            // staBegSta
+            // 
+            this.staBegSta.Location = new System.Drawing.Point(119, 20);
+            this.staBegSta.Name = "staBegSta";
+            this.staBegSta.Size = new System.Drawing.Size(111, 30);
+            this.staBegSta.Station = 0D;
+            this.staBegSta.TabIndex = 16;
+            // 
+            // labBegSta
+            // 
+            this.labBegSta.AutoSize = true;
+            this.labBegSta.Location = new System.Drawing.Point(24, 24);
+            this.labBegSta.Name = "labBegSta";
+            this.labBegSta.Size = new System.Drawing.Size(92, 17);
+            this.labBegSta.TabIndex = 15;
+            this.labBegSta.Text = "Begin Station";
             // 
             // txtNumBars
             // 
@@ -213,25 +265,43 @@
             this.labEndSta.TabIndex = 0;
             this.labEndSta.Text = "End Station";
             // 
+            // panCalcActions
+            // 
+            this.panCalcActions.Controls.Add(this.btnSpacing);
+            this.panCalcActions.Controls.Add(this.btnCalc);
+            this.panCalcActions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panCalcActions.Location = new System.Drawing.Point(3, 274);
+            this.panCalcActions.Name = "panCalcActions";
+            this.panCalcActions.Size = new System.Drawing.Size(516, 38);
+            this.panCalcActions.TabIndex = 2;
+            // 
+            // btnSpacing
+            // 
+            this.btnSpacing.Location = new System.Drawing.Point(350, 5);
+            this.btnSpacing.Name = "btnSpacing";
+            this.btnSpacing.Size = new System.Drawing.Size(157, 29);
+            this.btnSpacing.TabIndex = 5;
+            this.btnSpacing.Text = "Spacing Distances...";
+            this.btnSpacing.UseVisualStyleBackColor = true;
+            // 
             // btnCalc
             // 
-            this.btnCalc.Location = new System.Drawing.Point(218, 285);
+            this.btnCalc.Location = new System.Drawing.Point(215, 5);
             this.btnCalc.Name = "btnCalc";
-            this.btnCalc.Size = new System.Drawing.Size(86, 31);
-            this.btnCalc.TabIndex = 1;
+            this.btnCalc.Size = new System.Drawing.Size(86, 29);
+            this.btnCalc.TabIndex = 4;
             this.btnCalc.Text = "Calculate";
             this.btnCalc.UseVisualStyleBackColor = true;
-            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
             // 
             // grpResults
             // 
             this.grpResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)));
             this.grpResults.Controls.Add(this.lstVwResults);
-            this.grpResults.Location = new System.Drawing.Point(12, 316);
+            this.grpResults.Location = new System.Drawing.Point(3, 318);
             this.grpResults.Name = "grpResults";
-            this.grpResults.Size = new System.Drawing.Size(496, 404);
-            this.grpResults.TabIndex = 2;
+            this.grpResults.Size = new System.Drawing.Size(516, 369);
+            this.grpResults.TabIndex = 3;
             this.grpResults.TabStop = false;
             this.grpResults.Text = "Results";
             // 
@@ -252,9 +322,9 @@
             listViewGroup1,
             listViewGroup2});
             this.lstVwResults.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.lstVwResults.Location = new System.Drawing.Point(17, 23);
+            this.lstVwResults.Location = new System.Drawing.Point(9, 23);
             this.lstVwResults.Name = "lstVwResults";
-            this.lstVwResults.Size = new System.Drawing.Size(461, 367);
+            this.lstVwResults.Size = new System.Drawing.Size(498, 340);
             this.lstVwResults.TabIndex = 0;
             this.lstVwResults.UseCompatibleStateImageBehavior = false;
             this.lstVwResults.View = System.Windows.Forms.View.Details;
@@ -269,50 +339,40 @@
             this.cHdrValue.Text = "Value";
             this.cHdrValue.Width = 256;
             // 
-            // btnSpacing
+            // panActions
             // 
-            this.btnSpacing.Location = new System.Drawing.Point(344, 287);
-            this.btnSpacing.Name = "btnSpacing";
-            this.btnSpacing.Size = new System.Drawing.Size(157, 29);
-            this.btnSpacing.TabIndex = 3;
-            this.btnSpacing.Text = "Spacing Distances...";
-            this.btnSpacing.UseVisualStyleBackColor = true;
-            this.btnSpacing.Click += new System.EventHandler(this.btnSpacing_Click);
+            this.panActions.Controls.Add(this.btnAbout);
+            this.panActions.Controls.Add(this.btnClose);
+            this.panActions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panActions.Location = new System.Drawing.Point(3, 693);
+            this.panActions.Name = "panActions";
+            this.panActions.Size = new System.Drawing.Size(516, 39);
+            this.panActions.TabIndex = 4;
             // 
-            // labBegSta
+            // btnClose
             // 
-            this.labBegSta.AutoSize = true;
-            this.labBegSta.Location = new System.Drawing.Point(24, 24);
-            this.labBegSta.Name = "labBegSta";
-            this.labBegSta.Size = new System.Drawing.Size(92, 17);
-            this.labBegSta.TabIndex = 15;
-            this.labBegSta.Text = "Begin Station";
+            this.btnClose.Location = new System.Drawing.Point(221, 7);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 29);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
             // 
-            // staBegSta
+            // btnAbout
             // 
-            this.staBegSta.Location = new System.Drawing.Point(119, 20);
-            this.staBegSta.Name = "staBegSta";
-            this.staBegSta.Size = new System.Drawing.Size(111, 30);
-            this.staBegSta.Station = 0D;
-            this.staBegSta.TabIndex = 16;
-            // 
-            // staEndSta
-            // 
-            this.staEndSta.Location = new System.Drawing.Point(334, 20);
-            this.staEndSta.Name = "staEndSta";
-            this.staEndSta.Size = new System.Drawing.Size(111, 30);
-            this.staEndSta.Station = 0D;
-            this.staEndSta.TabIndex = 17;
+            this.btnAbout.Location = new System.Drawing.Point(432, 7);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(75, 29);
+            this.btnAbout.TabIndex = 1;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(522, 735);
-            this.Controls.Add(this.btnSpacing);
-            this.Controls.Add(this.grpResults);
-            this.Controls.Add(this.btnCalc);
-            this.Controls.Add(this.grpInput);
+            this.Controls.Add(this.tblLayMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(540, 900);
@@ -322,16 +382,31 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Crimson3DX Bar Spacing Calculator";
             this.Load += new System.EventHandler(this.OnLoad);
+            this.tblLayMain.ResumeLayout(false);
             this.grpInput.ResumeLayout(false);
             this.grpInput.PerformLayout();
+            this.panCalcActions.ResumeLayout(false);
             this.grpResults.ResumeLayout(false);
+            this.panActions.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TableLayoutPanel tblLayMain;
+        private System.Windows.Forms.GroupBox grpResults;
+        private System.Windows.Forms.ListView lstVwResults;
+        private System.Windows.Forms.ColumnHeader cHdrVariable;
+        private System.Windows.Forms.ColumnHeader cHdrValue;
         private System.Windows.Forms.GroupBox grpInput;
+        private C3DCommonCtrls.StationCtrl staEndSta;
+        private C3DCommonCtrls.StationCtrl staBegSta;
+        private System.Windows.Forms.Label labBegSta;
+        private System.Windows.Forms.TextBox txtNumBars;
+        private System.Windows.Forms.Label labNumBars;
+        private System.Windows.Forms.TextBox txtBundleCnt;
+        private System.Windows.Forms.Label labBundleCnt;
         private System.Windows.Forms.Label labEndClr;
         private C3DCommonCtrls.LengthTextBox lenEndClr;
         private System.Windows.Forms.Label labBegClr;
@@ -342,19 +417,13 @@
         private System.Windows.Forms.ComboBox cmbSpaOptions;
         private C3DCommonCtrls.BarSizeCtrl barSzCtrl;
         private System.Windows.Forms.Label labEndSta;
-        private System.Windows.Forms.Button btnCalc;
-        private System.Windows.Forms.GroupBox grpResults;
-        private System.Windows.Forms.ListView lstVwResults;
-        private System.Windows.Forms.ColumnHeader cHdrVariable;
-        private System.Windows.Forms.ColumnHeader cHdrValue;
-        private System.Windows.Forms.TextBox txtBundleCnt;
-        private System.Windows.Forms.Label labBundleCnt;
-        private System.Windows.Forms.TextBox txtNumBars;
-        private System.Windows.Forms.Label labNumBars;
+        private System.Windows.Forms.Panel panCalcActions;
         private System.Windows.Forms.Button btnSpacing;
-        private C3DCommonCtrls.StationCtrl staEndSta;
-        private C3DCommonCtrls.StationCtrl staBegSta;
-        private System.Windows.Forms.Label labBegSta;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Panel panActions;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button btnClose;
+
     }
 }
 
